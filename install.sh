cc893d916e9f2c89d21c09267cbdcd010db030c1
Philipp Melab <philipp.melab@gmail.com>
2023-08-03 11:21:20 +0200

    global git commands

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
+git config --global gpg.format ssh
+git config --global user.signingkey "$SSH_SIGNING_KEY"
+git config --global commit.gpgsign true
 
 alias wip="git commit --no-verify -m \"wip\""
-
