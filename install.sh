11fa2d2d4638ddd27f0804690fa10ed3f390ca12
Philipp Melab <philipp.melab@gmail.com>
2023-08-03 11:08:07 +0200

    move alias into install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
 
+alias wip="git commit --no-verify -m \"wip\""
+
