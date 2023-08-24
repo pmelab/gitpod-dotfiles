4907bbc74ec3a50e03263ac664473a775bc127e2
Philipp Melab <philipp.melab@gmail.com>
2023-08-24 13:21:24 +0200

    graphite auth

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ git config --global gpg.format ssh
 git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
 
-alias wip="git commit --no-verify -m \"wip\""
+cd $GITPOD_REPO_ROOT
+gt repo init --no-interactive
+gt auth --token $GRAPHITE_TOKEN
