ee9cdacf0f6fcec1558b88034f662992b928424c
Philipp Melab <philipp.melab@gmail.com>
2023-08-24 13:47:29 +0200

    logging

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+echo "Setting up commit signing with 1Password"
 git config --global gpg.format ssh
 git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
 
+echo "Install all files in home_files to the home directory"
 
 current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 dotfiles_source="${current_dir}/home_files"
@@ -21,6 +23,7 @@ while read -r file; do
 
 done < <(find "${dotfiles_source}" -type f)
 
+echo "Initialising Graphite"
 cd $GITPOD_REPO_ROOT
 gt repo init --no-interactive
 gt auth --token $GRAPHITE_TOKEN
