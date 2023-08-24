153dd813e85b139ef4f105a1f4f9b40cea8aae6f
Philipp Melab <philipp.melab@gmail.com>
2023-08-24 13:33:31 +0200

    install home_files

--- install.sh
+++ install.sh
@@ -2,6 +2,25 @@ git config --global gpg.format ssh
 git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
 
+
+current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+dotfiles_source="${current_dir}/home_files"
+
+while read -r file; do
+
+    relative_file_path="${file#"${dotfiles_source}"/}"
+    target_file="${HOME}/${relative_file_path}"
+    target_dir="${target_file%/*}"
+
+    if test ! -d "${target_dir}"; then
+        mkdir -p "${target_dir}"
+    fi
+
+    printf 'Installing dotfiles symlink %s\n' "${target_file}"
+    ln -sf "${file}" "${target_file}"
+
+done < <(find "${dotfiles_source}" -type f)
+
 cd $GITPOD_REPO_ROOT
 gt repo init --no-interactive
 gt auth --token $GRAPHITE_TOKEN
