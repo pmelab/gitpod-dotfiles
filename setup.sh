d8674856be0420dce5dbaa14cacfc2934e24bae3
Philipp Melab <philipp.melab@gmail.com>
2023-09-24 06:49:28 +0200

    Install neovim configuration

--- setup.sh
+++ setup.sh
@@ -5,23 +5,26 @@ git config --global gpg.format ssh
 git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
 
+echo "Install neovim configuration"
+git clone https://github.com/pmelab/nvim.git "${HOME}/.config/nvim"
+
 echo "Install all files in home_files to the home directory"
 
-current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+current_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 dotfiles_source="${current_dir}/home_files"
 
 while read -r file; do
 
-    relative_file_path="${file#"${dotfiles_source}"/}"
-    target_file="${HOME}/${relative_file_path}"
-    target_dir="${target_file%/*}"
+	relative_file_path="${file#"${dotfiles_source}"/}"
+	target_file="${HOME}/${relative_file_path}"
+	target_dir="${target_file%/*}"
 
-    if test ! -d "${target_dir}"; then
-        mkdir -p "${target_dir}"
-    fi
+	if test ! -d "${target_dir}"; then
+		mkdir -p "${target_dir}"
+	fi
 
-    printf 'Installing dotfiles symlink %s\n' "${target_file}"
-    ln -sf "${file}" "${target_file}"
+	printf 'Installing dotfiles symlink %s\n' "${target_file}"
+	ln -sf "${file}" "${target_file}"
 
 done < <(find "${dotfiles_source}" -type f)
 
